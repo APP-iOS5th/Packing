e7995712d0e323687da515995dbcd97c62c5ca48
JaeSunEo <wotjs020708@gmail.com>
2024-04-30 16:06:47 +0900

    Add AddJourneyView

---
+++ Packing/AddJourneyView.swift
@@ -0,0 +1,18 @@
+//
+//  AddJourneyView.swift
+//  Packing
+//
+//  Created by 어재선 on 4/30/24.
+//
+
+import SwiftUI
+
+struct AddJourneyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddJourneyView()
+}
