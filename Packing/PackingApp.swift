9c9418e7be0099bc9021357bd4cb281cefe95807
iyungui <rsdbddml@gmail.com>
2024-04-30 15:52:01 +0900

    Initialize Project Packing

---
+++ Packing/PackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackingApp.swift
+//  Packing
+//
+//  Created by 이융의 on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct PackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
