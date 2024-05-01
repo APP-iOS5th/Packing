193936f14348f9bcea1ceb75541a9c335102e3f0
JaeSunEo <wotjs020708@gmail.com>
2024-05-01 13:44:31 +0900

    사진 및 동영상 찰영 권한, 사진 동영상 첨부 사용권한 추가

--- Packing/AddJourneyView.swift
+++ Packing/AddJourneyView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct AddJourneyView: View {
     @State var testString = ""
-    @State private var Startdate = Date()
+    @State private var startdate = Date()
     @State private var endDate = Date()
-
+    
     @State private var selectedlist = ""
     private let list: [String] = ["test1","test2","test3","test4","test5"]
     
@@ -22,7 +22,7 @@ struct AddJourneyView: View {
                 VStack{
                     Spacer()
                     ZStack{
-                        RoundedRectangle(cornerRadius: 30)                            
+                        RoundedRectangle(cornerRadius: 30)
                             .foregroundStyle(.white)
                             .frame(height: 550)
                         VStack {
@@ -32,7 +32,7 @@ struct AddJourneyView: View {
                                 Text("여행목적")
                                     .font(.title3)
                                     .fontWeight(.bold)
-                                    
+                                
                                 TextField("여행목적",text: $testString)
                                     .padding()
                                     .font(.subheadline)
@@ -41,46 +41,42 @@ struct AddJourneyView: View {
                                     .frame(width: 300)
                                     .padding(.bottom,30)
                                 
-                                // MARK: 여행 기간
+                                // MARK: - 여행 기간
                                 Section(content: {
-                                    
-//                                    DatePicker(
-//                                        "Start Date",
-//                                        selection: $date,
-//                                        displayedComponents: [.date]
-//                                    )
-//                                    .background(Color(hex: 0xF3F3F3))
-//                                    .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
-//                                    .datePickerStyle(.graphical)
-//                                    .frame(width: 300)
-//                                    .bold()
-                                    DatePicker("시작 날짜", selection: $Startdate, displayedComponents: [.date])
+                                                                        DatePicker("시작 날짜", selection: $startdate, displayedComponents: [.date])
+
                                     DatePicker("종료 날짜", selection: $endDate, displayedComponents: [.date])
+                                        .padding(.bottom,30)
                                 }, header: {
                                     Text("여행 기간")
                                         .font(.title3)
                                         .fontWeight(.bold)
                                     
                                 })
-                                .frame(width: 300 , alignment: .leading)
-                        
-                                    
                                 
-                                //MARK: 여행 목적
-                                Text("여행 활동")
-                                    .font(.title3)
-                                    .fontWeight(.bold)
-                                    .padding(.top,30)
-                                Picker("list", selection: $selectedlist){
+                                .frame(width: 300 , alignment: .leading)
+        
+                                
+                                
+                                //MARK: - 여행 목적
+                                Picker("여행 활동", selection: $selectedlist){
                                     ForEach(list, id: \.self) {
                                         Text($0)
-                                        .clipShape(RoundedRectangle(cornerRadius: 15.0))
-                                    }.pickerStyle(.wheel)
-                                }.background(Color(hex: 0xF3F3F3))
+                                    }
+                                }
+                                .foregroundStyle(.black)
+                                .font(.title3)
+                                .bold()
+                                .padding()
+                                .pickerStyle(.navigationLink)
+                                .frame(width: 300,height: 60)
+                                .background(Color(hex: 0xF3F3F3))
+                                .clipShape(RoundedRectangle(cornerRadius: 15.0))
+                                
                                 
                             }.padding(.bottom,30)
                             
-                            //MARK: 확인 버튼
+                            //MARK: - 확인 버튼
                             Button{
                                 //TODO: 버튼 클릭시 데이터 전송
                             } label: {
@@ -91,15 +87,27 @@ struct AddJourneyView: View {
                                     .padding(.vertical,20)
                                     .background(Color(hex: 0x566375))
                                     .clipShape(RoundedRectangle(cornerRadius: 15.0))
-                            
+                                
                             }
-
+                            
                         }
                     }
                 }
             }
             .ignoresSafeArea(.all)
-            
+        }
+        .toolbar{
+            ToolbarItem(placement: .topBarTrailing) {
+                //MARK: - 사진 추가
+                Button{
+                    //TODO: 사진 추가 기능 구현
+                }label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .foregroundStyle(Color(hex: 0x566375))
+                }
+            }
         }
         
     }
@@ -107,7 +115,7 @@ struct AddJourneyView: View {
 
 #Preview {
     NavigationStack{
-
+        
         AddJourneyView()
     }
 }
