1338d2c0499a309a15730065d08a8fa2f13a4adb
iyungui <rsdbddml@gmail.com>
2024-05-02 13:29:25 +0900

    update JourneyService and Add fetch to JourneyListView

--- Packing/JourneyListView.swift
+++ Packing/JourneyListView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-enum TravelActivity: String, CaseIterable {
+enum TravelActivity: String, CaseIterable, Codable {
     case beach = "해변"
     case camping = "캠핑"
     case hiking = "등산"
@@ -21,8 +21,8 @@ enum TravelActivity: String, CaseIterable {
 }
 
 
-struct Journey: Identifiable, Hashable {
-    let id: UUID = UUID()
+struct Journey: Identifiable, Codable, Hashable {
+    var id: UUID = UUID()
     let destination: String // 여행 목적지
     let activities: [TravelActivity]
     let image: String    //  여행 사진
@@ -47,91 +47,20 @@ extension Journey {
     ]
 }
 
-//
-//struct JourneyListView: View {
-//    var journeys = Journey.sample
-//    @State private var isNewJourneyPresented = false
-//
-//    var body: some View {
-//        NavigationStack {
-//            ZStack {
-////                Color("mainColor")
-//
-//                VStack {
-//                    if journeys.isEmpty {
-//                        Image(systemName: "airplane")
-//                            .font(.title)
-//                            .padding()
-//
-//                        Text("현재 여행 목록이 없습니다.\n여행을 추가해주세요.")
-//                            .font(.headline)
-//                            .multilineTextAlignment(.center)
-//                    } else {
-//                        List(journeys) { journey in
-//                            NavigationLink(value: journey) {
-//                                JourneySummaryView(journey: journey)
-//                                    .frame(height: 100)
-//                            }
-//                            .listRowSeparator(.hidden)
-//                            .cornerRadius(3.0)
-//                            .shadow(radius: 3, x: 3, y: 3)
-//                        }
-//                        .listStyle(.plain)
-//                        .navigationDestination(for: Journey.self) { journey in
-//                            JourneyDetailView(journey: journey)
-//                        }
-//                    }
-//                }
-//
-//                // MARK: - ADD BUTTON
-//                VStack {
-//                    Spacer()
-//                    HStack {
-//                        Spacer()
-//                        Button {
-//                            isNewJourneyPresented.toggle()
-//                        } label: {
-//                            Image(systemName: "bag.fill.badge.plus")
-//                                .font(.largeTitle)
-//                                .foregroundStyle(Color("mainColor"))
-//                                .shadow(radius: 1)
-//                        }
-//                    }
-//                    .padding(.horizontal)
-//                }
-//                .padding()
-//            }
-//
-//            .navigationTitle("JourneyListView")
-//            .navigationBarTitleDisplayMode(.inline)
-//            .toolbarBackground(Color("mainColor"), for: .navigationBar)
-//            .sheet(isPresented: $isNewJourneyPresented) {
-//                // MARK: -  AddJourneyView 추가
-//                EmptyView()
-//                    .presentationDetents([.medium, .large])
-//                    .presentationCornerRadius(21)
-//            }
-//        }
-//    }
-//}
-
 struct JourneyListView: View {
-    var journeys = Journey.sample
+    @StateObject private var service: JourneyService = JourneyService()
+//    var journeys = Journey.sample
     @State private var selectedJourney: Journey?
-    
-    @State private var isNewJourneyPresented = false
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
         NavigationStack {
             ZStack {
-                //                Color("mainColor").ignoresSafeArea()
-                
                 LinearGradient(gradient: Gradient(colors: colorScheme == .light ? [Color(hex: "AEC6CF"), Color(hex: "ECECEC"), Color(hex: "FFFDD0")] : [Color(hex: "34495E"), Color(hex: "555555"), Color(hex: "333333")]), startPoint: .topLeading, endPoint: .bottomTrailing)
                     .ignoresSafeArea()
                 
                 VStack {
-                    if journeys.isEmpty {
+                    if service.journeys.isEmpty {
                         Image(systemName: "airplane")
                             .font(.title)
                             .padding()
@@ -148,7 +77,7 @@ struct JourneyListView: View {
                                 .shadow(radius: 5)
                                 .edgesIgnoringSafeArea(.bottom)
                             
-                            List(journeys) { journey in
+                            List(service.journeys) { journey in
                                 Button(action: {
                                     self.selectedJourney = journey
                                 }) {
@@ -175,48 +104,19 @@ struct JourneyListView: View {
                         //                        .offset(y: 20)
                     }
                 }
-                //                // MARK: - ADD BUTTON
-                //                VStack {
-                //                    Spacer()
-                //                    HStack {
-                //                        Spacer()
-                //                        Button {
-                //                            isNewJourneyPresented.toggle()
-                //                        } label: {
-                //                            Image(systemName: "bag.fill.badge.plus")
-                //                                .font(.largeTitle)
-                //                                .foregroundStyle(Color("mainColor"))
-                ////                                .shadow(radius: 1)
-                //                        }
-                //                    }
-                //                    .padding(.horizontal)
-                //                }
-                //                .padding()
             }
-            //            .navigationTitle("여행 목록")
             .toolbarBackground(Color("mainColor"), for: .navigationBar)
-            .sheet(isPresented: $isNewJourneyPresented) {
-                // MARK: -  AddJourneyView 추가
-                EmptyView()
-                    .presentationDetents([.medium, .large])
-                    .presentationCornerRadius(21)
-            }
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
-                    //                    Button {
-                    //                        isNewJourneyPresented.toggle()
-                    //
-                    //                    } label: {
-                    //                        Image(systemName: "bag.fill.badge.plus")
-                    //                            .font(.title)
-                    //                            .foregroundStyle(Color("DarkColor"))
-                    //                    }
                     NavigationLink(destination: AddJourneyView()){
                         Image(systemName: "bag.fill.badge.plus")
                             .font(.title)
                             .foregroundStyle(Color("DarkColor"))
                     }
                 }
+            }
+            .task {
+                service.fetch()
             }
         }
     }
@@ -283,6 +183,7 @@ struct JourneyDetailView: View {
 
 #Preview {
     JourneyListView()
+        .environmentObject(JourneyService())
 }
 
 extension Color {
--- Packing/JourneyService.swift
+++ Packing/JourneyService.swift
@@ -6,7 +6,7 @@
 //
 
 import Firebase
-import FirebaseCore
+import FirebaseFirestore
 
 class JourneyService: ObservableObject {
     @Published var journeys: [Journey]
@@ -15,7 +15,71 @@ class JourneyService: ObservableObject {
     
     init(journeys: [Journey] = []) {
         self.journeys = journeys
-//        startRealtimeUpdates()
+        startRealtimeUpdates()
+    }
+    
+    func fetch() {
+        guard listener == nil else { return }
+        dbCollection.getDocuments { [self] querySnapshot, error in
+            guard let snapshot = querySnapshot else {
+                print("Error fetching snapshots: \(error!)")
+                return
+            }
+            updateJourneys(snapshot: snapshot)
+        }
+    }
+    
+    // Adds a new journey to Firestore
+    func addJourney(destination: String, activities: [String], image: String, startDate: Date, endDate: Date, packingItemId: String) {
+        let newJourney = Journey(
+            id: UUID(),
+            destination: destination,
+            activities: activities.compactMap { TravelActivity(rawValue: $0) },
+            image: image,
+            startDate: startDate,
+            endDate: endDate,
+            packingItemId: packingItemId
+        )
+        
+        _ = try? dbCollection.addDocument(from: newJourney) { error in
+            if let error = error {
+                print("Error adding journey: \(error)")
+            } else {
+                self.fetch() // Optionally fetch all journeys again to refresh the local data
+            }
+        }
+    }
+    
+    // Starts listening for real-time updates to the journeys collection
+    private func startRealtimeUpdates() {
+        listener = dbCollection.addSnapshotListener { [self] querySnapshot, error in
+            guard let snapshot = querySnapshot else {
+                print("Error fetching snapshots: \(error!)")
+                return
+            }
+            snapshot.documentChanges.forEach { diff in
+                if (diff.type == .added) {
+                    print("New journey added: \(diff.document.data())")
+                }
+                if (diff.type == .modified) {
+                    print("Modified journey: \(diff.document.data())")
+                }
+                if (diff.type == .removed) {
+                    print("Removed journey: \(diff.document.data())")
+                }
+            }
+            updateJourneys(snapshot: snapshot)
+        }
+    }
+    
+    // Updates the local journeys array with the latest data from Firestore
+    private func updateJourneys(snapshot: QuerySnapshot) {
+        let journeys: [Journey] = snapshot.documents.compactMap { document in
+            var journey = try? document.data(as: Journey.self)
+            journey?.id = UUID(uuidString: document.documentID) ?? UUID()
+            return journey
+        }
+        self.journeys = journeys.sorted(by: { $0.startDate > $1.startDate })
     }
 }
 
